b14082d5832a40bf7d695db299c029b5fa9e59d9
Elias <caprisnoctis@googlemail.com>
2021-02-03 19:26:14 +0100

    initial project upload

---
+++ Assets/GridMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMaster : MonoBehaviour
+{
+    void Start()
+    {
+        // TODO:
+
+        // Init Hexagonal Grid with row, column vars
+        // each tile saves:
+            // its pos in grid
+            // its current type/color
+            // its neighbour tiles
+
+        // IDEAS:
+
+        /*
+         * Place Models on Hexagons
+         * Grid Snapping for placing models on hexagons
+         * Biome Logic: eg. Hexagons will init as lake/ water, hills etc in sensible pattern
+         * Water tiles -> rivers form from spring to edge of tiles -> fall as waterfall
+         */
+    }
+
+    void Update()
+    {
+        
+    }
+}
